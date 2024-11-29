71ea24baf47fabe85303182d0ce26ed1de911f0b
Harivignesh-33 <harivigneshari61@gmail.com>
2024-11-29 19:30:51 +0530

    first commit

---
+++ Atoi.java
@@ -0,0 +1,25 @@
+//?  Implement Atoi 
+
+class Solution {
+    public int myAtoi(String s) {
+        // Your code here
+         int sign = 1, result = 0, index = 0, INT_MAX = 2147483647, INT_MIN = -2147483648;
+
+        while (index < s.length() && s.charAt(index) == ' ') {
+            index++; 
+        }
+        
+        if (index < s.length() && (s.charAt(index) == '+' || s.charAt(index) == '-'))
+            sign = (s.charAt(index++) == '-') ? -1 : 1; 
+
+        while (index < s.length() && s.charAt(index) >= '0' && s.charAt(index) <= '9') {
+         
+            int digit = s.charAt(index++) - '0'; 
+         
+            if (result > (INT_MAX - digit) / 10) return (sign == 1) ? INT_MAX : INT_MIN; // Check overflow
+         
+            result = result * 10 + digit; 
+        }
+        return result * sign; 
+    }
+}
---
+++ BinaryStr.java
@@ -0,0 +1,47 @@
+// Add binary strings 
+//{ Driver Code Starts
+// Initial Template for Java
+
+// ? Add binary strings
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public String addBinary(String s1, String s2) {
+        // code here
+         StringBuilder ans = new StringBuilder();
+        int carry = 0;
+
+        int i = s1.length() - 1;
+        int j = s2.length() - 1;
+
+        
+        while (i >= 0 || j >= 0 || carry > 0) {
+            int val = carry;
+            if (i >= 0) val += s1.charAt(i--) - '0';
+            if (j >= 0) val += s2.charAt(j--) - '0';
+
+            carry = val / 2;
+            ans.append(val % 2); // adding the current digit
+        }
+
+        // Reverse 
+        ans.reverse();
+
+        // remove the leading zeros
+        int startIndex = 0;
+        while (startIndex < ans.length() && ans.charAt(startIndex) == '0') {
+            startIndex++;
+        }
+
+        // If all zeros, return "0"
+        if (startIndex == ans.length()) {
+            return "0";
+        }
+
+        return ans.substring(startIndex);
+    }
+}
+
